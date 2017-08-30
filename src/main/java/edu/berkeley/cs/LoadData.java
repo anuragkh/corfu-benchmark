8bc4ef21bc0c9a71de5ce8a74a989145586420c5
Anurag Khandelwal <anuragk@berkeley.edu>
2017-08-30 15:03:30 -0700

    Updated data load

--- src/main/java/edu/berkeley/cs/LoadData.java
+++ src/main/java/edu/berkeley/cs/LoadData.java
@@ -2,18 +2,24 @@ package edu.berkeley.cs;
 
 @Deprecated
 public class LoadData extends CorfuBenchmark {
-    public LoadData(String conf, int batchSize, int numBatches, int numThreads, String dataSource) {
+    LoadData(String conf, int batchSize, int numBatches, int numThreads, String dataSource) {
         super(conf, batchSize, numBatches, numThreads, dataSource);
     }
 
     void runBenchmark() {
         LOG.info("Loading data to " + getStreamName());
         long startTime = System.currentTimeMillis();
-        TXBegin();
-        for (int i = 0; i < getNumDataPts(); i++) {
-            getMap().put(dataPoint(i).timestamp, dataPoint(i).value);
+        for (int i = 0; i < getNumDataPts(); i += getBatchSize()) {
+            TXBegin();
+            for (int j = 0; j < getBatchSize(); j++) {
+                DataPoint p = dataPoint(i + j);
+                getMap().put(p.timestamp, p.value);
+            }
+            TXEnd();
+            if (i != 0 && i % 10000 == 0) {
+                LOG.info("Loaded " + i + " data points...");
+            }
         }
-        TXEnd();
         long endTime = System.currentTimeMillis();
         long totTime = endTime - startTime;
         LOG.info("Load took " + totTime + " ms");
